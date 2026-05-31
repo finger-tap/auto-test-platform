7f9224a0678af71245dfda750925cb3356ef017c
dinghao <dinghao_it@qq.com>
2026-05-31 18:51:39 +0800

    fix: App.tsx 移除 ScheduleList 的 testType prop

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -19,11 +19,21 @@ import ScenarioSetDetail from './pages/scenario-set/ScenarioSetDetail';
 import MockList from './pages/mock/MockList';
 import MockDetail from './pages/mock/MockDetail';
 import BatchReportList from './pages/batch-report/BatchReportList';
+import BatchReportDetail from './pages/batch-report/BatchReportDetail';
 import ScheduleList from './pages/schedule/ScheduleList';
 import ScheduleDetail from './pages/schedule/ScheduleDetail';
 import EnvironmentList from './pages/environment/EnvironmentList';
 import EnvironmentDetail from './pages/environment/EnvironmentDetail';
-import Placeholder from './pages/Placeholder';
+import SystemConfig from './pages/system/SystemConfig';
+import MobileTestHome from './pages/mobile-test/MobileTestHome';
+import MobileTestList from './pages/mobile-test/MobileTestList';
+import MobileTestDetail from './pages/mobile-test/MobileTestDetail';
+import WebTestHome from './pages/web-test/WebTestHome';
+import WebCaseList from './pages/web-test/WebCaseList';
+import WebCaseDetail from './pages/web-test/WebCaseDetail';
+import PcTestHome from './pages/pc-test/PcTestHome';
+import PcCaseList from './pages/pc-test/PcCaseList';
+import PcCaseDetail from './pages/pc-test/PcCaseDetail';
 import Profile from './pages/Profile';
 import './App.css';
 
@@ -51,27 +61,76 @@ function App() {
                 <Route path="/api-test/api-case" element={<ApiList />} />
                 <Route path="/api-test/api-case/new" element={<ApiDetail />} />
                 <Route path="/api-test/api-case/:id" element={<ApiDetail />} />
-                <Route path="/api-test/scene-case" element={<ScenarioList />} />
-                <Route path="/api-test/scene-case/:id" element={<ScenarioDetail />} />
-                <Route path="/api-test/scene-set" element={<ScenarioSetList />} />
-                <Route path="/api-test/scene-set/:id" element={<ScenarioSetDetail />} />
-                <Route path="/api-test/schedule" element={<ScheduleList />} />
-                <Route path="/api-test/schedule/:id" element={<ScheduleDetail />} />
-                <Route path="/api-test/batch-report" element={<BatchReportList />} />
-                <Route path="/api-test/mock" element={<MockList />} />
-                <Route path="/api-test/mock/:id" element={<MockDetail />} />
-                <Route path="/api-test/environment" element={<EnvironmentList />} />
-                <Route path="/api-test/environment/new" element={<EnvironmentDetail />} />
-                <Route path="/api-test/environment/:id" element={<EnvironmentDetail />} />
+                <Route path="/api-test/scene-case" element={<ScenarioList basePath="/api-test" testType="api" />} />
+                <Route path="/api-test/scene-case/:id" element={<ScenarioDetail basePath="/api-test" testType="api" />} />
+                <Route path="/api-test/scene-set" element={<ScenarioSetList basePath="/api-test" testType="api" />} />
+                <Route path="/api-test/scene-set/:id" element={<ScenarioSetDetail basePath="/api-test" testType="api" />} />
+                <Route path="/api-test/schedule" element={<ScheduleList basePath="/api-test" />} />
+                <Route path="/api-test/schedule/:id" element={<ScheduleDetail testType="api" />} />
+                <Route path="/api-test/batch-report" element={<BatchReportList basePath="/api-test" testType="api" />} />
+                <Route path="/api-test/batch-report/:id" element={<BatchReportDetail testType="api" />} />
+                <Route path="/api-test/mock" element={<MockList basePath="/api-test" testType="api" />} />
+                <Route path="/api-test/mock/:id" element={<MockDetail testType="api" />} />
+                <Route path="/api-test/environment" element={<EnvironmentList basePath="/api-test" testType="api" />} />
+                <Route path="/api-test/environment/new" element={<EnvironmentDetail testType="api" />} />
+                <Route path="/api-test/environment/:id" element={<EnvironmentDetail testType="api" />} />
+                <Route path="/api-test/system-config" element={<SystemConfig />} />
                 
                 {/* === Web测试 /web-test === */}
-                <Route path="/web-test" element={<Placeholder title="Web测试" />} />
+                <Route path="/web-test" element={<WebTestHome />} />
+                <Route path="/web-test/case" element={<WebCaseList />} />
+                <Route path="/web-test/case/new" element={<WebCaseDetail />} />
+                <Route path="/web-test/case/:id" element={<WebCaseDetail />} />
+                <Route path="/web-test/scene" element={<ScenarioList basePath="/web-test" testType="web" />} />
+                <Route path="/web-test/scene/:id" element={<ScenarioDetail basePath="/web-test" testType="web" />} />
+                <Route path="/web-test/scene-set" element={<ScenarioSetList basePath="/web-test" testType="web" />} />
+                <Route path="/web-test/scene-set/:id" element={<ScenarioSetDetail basePath="/web-test" testType="web" />} />
+                <Route path="/web-test/schedule" element={<ScheduleList basePath="/web-test" />} />
+                <Route path="/web-test/schedule/:id" element={<ScheduleDetail testType="web" />} />
+                <Route path="/web-test/batch-report" element={<BatchReportList basePath="/web-test" testType="web" />} />
+                <Route path="/web-test/batch-report/:id" element={<BatchReportDetail testType="web" />} />
+                <Route path="/web-test/environment" element={<EnvironmentList basePath="/web-test" testType="web" />} />
+                <Route path="/web-test/environment/new" element={<EnvironmentDetail testType="web" />} />
+                <Route path="/web-test/environment/:id" element={<EnvironmentDetail testType="web" />} />
+                <Route path="/web-test/system-config" element={<SystemConfig />} />
                 
                 {/* === 移动端测试 /mobile-test === */}
-                <Route path="/mobile-test" element={<Placeholder title="移动端测试" />} />
+                <Route path="/mobile-test" element={<MobileTestHome />} />
+                <Route path="/mobile-test/test-case" element={<MobileTestList />} />
+                <Route path="/mobile-test/test-case/new" element={<MobileTestDetail />} />
+                <Route path="/mobile-test/test-case/:id" element={<MobileTestDetail />} />
+                <Route path="/mobile-test/scene-case" element={<ScenarioList basePath="/mobile-test" testType="mobile" />} />
+                <Route path="/mobile-test/scene-case/:id" element={<ScenarioDetail basePath="/mobile-test" testType="mobile" />} />
+                <Route path="/mobile-test/scene-set" element={<ScenarioSetList basePath="/mobile-test" testType="mobile" />} />
+                <Route path="/mobile-test/scene-set/:id" element={<ScenarioSetDetail basePath="/mobile-test" testType="mobile" />} />
+                <Route path="/mobile-test/schedule" element={<ScheduleList basePath="/mobile-test" />} />
+                <Route path="/mobile-test/schedule/:id" element={<ScheduleDetail testType="mobile" />} />
+                <Route path="/mobile-test/batch-report" element={<BatchReportList basePath="/mobile-test" testType="mobile" />} />
+                <Route path="/mobile-test/batch-report/:id" element={<BatchReportDetail testType="mobile" />} />
+                <Route path="/mobile-test/mock" element={<MockList basePath="/mobile-test" testType="mobile" />} />
+                <Route path="/mobile-test/mock/:id" element={<MockDetail testType="mobile" />} />
+                <Route path="/mobile-test/environment" element={<EnvironmentList basePath="/mobile-test" testType="mobile" />} />
+                <Route path="/mobile-test/environment/new" element={<EnvironmentDetail testType="mobile" />} />
+                <Route path="/mobile-test/environment/:id" element={<EnvironmentDetail testType="mobile" />} />
+                <Route path="/mobile-test/system-config" element={<SystemConfig />} />
                 
                 {/* === PC测试 /pc-test === */}
-                <Route path="/pc-test" element={<Placeholder title="PC测试" />} />
+                <Route path="/pc-test" element={<PcTestHome />} />
+                <Route path="/pc-test/case" element={<PcCaseList />} />
+                <Route path="/pc-test/case/new" element={<PcCaseDetail />} />
+                <Route path="/pc-test/case/:id" element={<PcCaseDetail />} />
+                <Route path="/pc-test/scene" element={<ScenarioList basePath="/pc-test" testType="pc" />} />
+                <Route path="/pc-test/scene/:id" element={<ScenarioDetail basePath="/pc-test" testType="pc" />} />
+                <Route path="/pc-test/scene-set" element={<ScenarioSetList basePath="/pc-test" testType="pc" />} />
+                <Route path="/pc-test/scene-set/:id" element={<ScenarioSetDetail basePath="/pc-test" testType="pc" />} />
+                <Route path="/pc-test/schedule" element={<ScheduleList basePath="/pc-test" />} />
+                <Route path="/pc-test/schedule/:id" element={<ScheduleDetail testType="pc" />} />
+                <Route path="/pc-test/batch-report" element={<BatchReportList basePath="/pc-test" testType="pc" />} />
+                <Route path="/pc-test/batch-report/:id" element={<BatchReportDetail testType="pc" />} />
+                <Route path="/pc-test/environment" element={<EnvironmentList basePath="/pc-test" testType="pc" />} />
+                <Route path="/pc-test/environment/new" element={<EnvironmentDetail testType="pc" />} />
+                <Route path="/pc-test/environment/:id" element={<EnvironmentDetail testType="pc" />} />
+                <Route path="/pc-test/system-config" element={<SystemConfig />} />
               </Route>
             </Route>
             <Route path="*" element={<Navigate to="/" replace />} />
